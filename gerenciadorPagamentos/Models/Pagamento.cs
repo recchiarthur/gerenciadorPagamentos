1f0e1bcfd37fd316561fbdb85bf947924fd32077
recchiarthur <recchiarthur@gmail.com>
2022-05-24 22:03:07 -0300

    Adicionar arquivos de projeto.

---
+++ gerenciadorPagamentos/Models/Pagamento.cs
@@ -0,0 +1,13 @@
+﻿namespace gerenciadorPagamentos.Models
+{
+    public class Pagamento
+    {
+        public int Id { get; set; }
+
+        public string descricao { get; set; }
+
+        public float valor { get; set; }
+
+
+    }
+}
